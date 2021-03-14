0bdb5acdbcacb7c94506bd92be3ee91fbe874575
Isakiye Afasha <afashaisakiye@gmail.com>
2021-03-14 08:12:56 +0300

    Updates

---
+++ precache-manifest.8fec829a00ab35be61516781e60fb726.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff7281f0588d519bc7bfcc04ec3ef24",
+    "url": "/kampala/index.html"
+  },
+  {
+    "revision": "63544532e4f01909ce60",
+    "url": "/kampala/static/css/main.c6604ed8.chunk.css"
+  },
+  {
+    "revision": "41bacfea89da875a468d",
+    "url": "/kampala/static/js/2.59d563a4.chunk.js"
+  },
+  {
+    "revision": "ce15a0ffcfe8b91e4f219c1d7bb8c5ef",
+    "url": "/kampala/static/js/2.59d563a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63544532e4f01909ce60",
+    "url": "/kampala/static/js/main.95e8d4c0.chunk.js"
+  },
+  {
+    "revision": "ddeb722d76dec13628e6",
+    "url": "/kampala/static/js/runtime-main.89b4cdbf.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/kampala/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "0f9928d77bc67692e90527ec092fcbd0",
+    "url": "/kampala/static/media/feature-tile-icon-01.0f9928d7.svg"
+  },
+  {
+    "revision": "bd39f30434175e7c8b1d1c74fa6569fd",
+    "url": "/kampala/static/media/feature-tile-icon-02.bd39f304.svg"
+  },
+  {
+    "revision": "66f37ba5c7e4b6648f04883fa637e634",
+    "url": "/kampala/static/media/feature-tile-icon-03.66f37ba5.svg"
+  },
+  {
+    "revision": "836acd100a942420f8ab4e6c5e4162d8",
+    "url": "/kampala/static/media/feature-tile-icon-04.836acd10.svg"
+  },
+  {
+    "revision": "fa9ba00b178abc7bd2e02bd986e91fe3",
+    "url": "/kampala/static/media/feature-tile-icon-05.fa9ba00b.svg"
+  },
+  {
+    "revision": "6a1776963deb39d710bae80bb449aebe",
+    "url": "/kampala/static/media/feature-tile-icon-06.6a177696.svg"
+  },
+  {
+    "revision": "d9cb99ceb6a66250792ed78f5765325b",
+    "url": "/kampala/static/media/features-split-image-01.d9cb99ce.png"
+  },
+  {
+    "revision": "3c569239f88160018f79029ba5dc227d",
+    "url": "/kampala/static/media/features-split-image-02.3c569239.png"
+  },
+  {
+    "revision": "87e4d053756c60c9f421195d8eafdbfe",
+    "url": "/kampala/static/media/features-split-image-03.87e4d053.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/kampala/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/kampala/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "ef999002df79edb1d142155ce1dcd81e",
+    "url": "/kampala/static/media/jslogo.ef999002.jpeg"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/kampala/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
